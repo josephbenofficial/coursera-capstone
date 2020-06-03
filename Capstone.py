140d04de145c1edb6934edec3837db5d435c9553
josephbenofficial <66313320+josephbenofficial@users.noreply.github.com>
2020-06-03 16:14:57 +0530

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook to be used for the final capstone project
+# 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project")
+
+
+# In[ ]:
+
+
+
+
